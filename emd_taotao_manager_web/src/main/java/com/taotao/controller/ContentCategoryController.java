2debc8fa3affbbc0700e97d0e013ecf7241bc652
qq986945193 <986945193@qq.com>
2017-12-29 16:35:13 +0800

    commit portal

---
+++ emd_taotao_manager_web/src/main/java/com/taotao/controller/ContentCategoryController.java
@@ -0,0 +1,14 @@
+package com.taotao.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * 内容分类管理
+ * @Author ：David
+ * @新浪微博 ：http://weibo.com/mcxiaobing
+ * @GitHub: https://github.com/QQ986945193
+ */
+@Controller
+public class ContentCategoryController {
+
+}

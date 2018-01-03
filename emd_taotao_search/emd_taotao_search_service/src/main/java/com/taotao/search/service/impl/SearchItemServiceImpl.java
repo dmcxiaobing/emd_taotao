14f0fb1230a28b860f42d9703b67e510c1adba03
qq986945193 <986945193@qq.com>
2018-01-03 17:26:13 +0800

    commit solr

--- emd_taotao_search/emd_taotao_search_service/src/main/java/com/taotao/search/service/impl/SearchItemServiceImpl.java
+++ emd_taotao_search/emd_taotao_search_service/src/main/java/com/taotao/search/service/impl/SearchItemServiceImpl.java
@@ -1,5 +1,7 @@
 package com.taotao.search.service.impl;
 
+import com.taotao.search.service.SearchItemService;
+
 public class SearchItemServiceImpl implements SearchItemService{
 
 }
---
+++ emd_taotao_search/emd_taotao_search_service/src/test/java/com/taotao/solrj/TestSolrJ.java
@@ -0,0 +1,20 @@
+package com.taotao.solrj;
+
+import org.junit.Test;
+
+/**
+ * solrJ的简单测试
+ * @Author ：David
+ * @新浪微博 ：http://weibo.com/mcxiaobing
+ * @GitHub: https://github.com/QQ986945193
+ */
+public class TestSolrJ {
+
+	/**
+	 * 添加索引内容
+	 */
+	@Test
+	public void testAddDocument() {
+		
+	}
+}
